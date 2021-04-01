72f1be3288aa023aae0d1e99df8f90ed73fd9d7e
7298cndnj <7298cndnj@naver.com>
2021-04-01 18:16:07 +0900

    chap12 4/1 확인문제

---
+++ chap12 javaLab/src/verify/exam3/ThreadExample.java
@@ -0,0 +1,15 @@
+package verify.exam3;
+
+public class ThreadExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread = new MovieThread();   
+		thread.setDaemon(true);  
+		thread.start();      
+		try { Thread. sleep (3000); 
+		} catch (InterruptedException e) {
+			
+		}  
+		}
+}
