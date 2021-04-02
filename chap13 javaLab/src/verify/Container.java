e3ba12204e5aa91b4c05feb5c394ebd02f197d8e
7298cndnj <7298cndnj@naver.com>
2021-04-02 15:03:47 +0900

    chap14 수업 예제

---
+++ chap13 javaLab/src/verify/Container.java
@@ -0,0 +1,18 @@
+package verify;
+
+public class Container<K,V>{
+	private K key;
+	private V value;
+public K getKey() {
+	return this.key;
+}
+public V getValue() {
+	return this.value;
+}
+public void setKey(K key) {
+	this.key=key;
+}
+public void setValue(V value) {
+	this.value=value;
+}
+}
