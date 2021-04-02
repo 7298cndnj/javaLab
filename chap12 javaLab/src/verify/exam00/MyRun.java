25d0b72e93dc2463266d48f4eca095be79965c59
7298cndnj <7298cndnj@naver.com>
2021-04-02 11:37:26 +0900

    chap13 0402 수업예제

---
+++ chap12 javaLab/src/verify/exam00/MyRun.java
@@ -0,0 +1,6 @@
+package verify.exam00;
+//인터페이스 Runnable 구현클래스로 생성
+//작성위치
+public class MyRun {
+
+}
---
+++ chap12 javaLab/src/verify/exam00/MyThread.java
@@ -0,0 +1,8 @@
+package verify.exam00;
+
+
+//Thread 클래스를 상속한 하위클래스로 생성
+//작성위치
+public class MyThread {
+
+}
