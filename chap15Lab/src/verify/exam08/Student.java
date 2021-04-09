6bae365792ab90e066d7e5188cc13c35e3b368cf
7298cndnj <7298cndnj@naver.com>
2021-04-09 11:38:44 +0900

    chap16 수업예제

--- chap15Lab/src/verify/exam08/Student.java
+++ chap15Lab/src/verify/exam08/Student.java
@@ -3,22 +3,25 @@ package verify.exam08;
 public class Student {
 	public int studentNum;
 	public String name;
-	
-	public Student (int studentNum, String name) {
+
+	public Student(int studentNum, String name) {
 		this.studentNum = studentNum;
 		this.name = name;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return studentNum;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if(!(obj instanceof Student)) return false;
-		Student student = (Student) obj;
-		if(studentNum != student.studentNum) return false;
-		return true;
+		// #2 작성 위치
+		if(obj instanceof Student) {
+			Student student=(Student)obj;
+			if(studentNum==student.studentNum)
+				return true;
+		}
+		return false;
 	}
 }
